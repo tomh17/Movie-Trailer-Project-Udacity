94ae505d2404603eea91e1dc2dc22052147b1b52
Tom <tom.horsfall.2015@owu.edu>
2018-06-22 01:23:21 -0400

    Followed through on Pep Guidelines

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,47 +1,69 @@
 import fresh_tomatoes
-import media   #importing media.py from file in the same folder (Movie Trailer Project)
+import media   # importing media.py from file in the
+# same folder (Movie Trailer Project)
 
-#the next six variables are instances of the class "Movie" from media.py
-#each of which represent the title, storyline, url for their poster image
-#as well as a link to their youtube trailer. When run, this file will create
-#an html document alongside media.py and fresh tomatoes.py that will display the name and 
-#movie poster for each movie listed below. If the image for a particular movie is clicked,
-#it will begin playing the youtube movie trailer for that film. 
+# the next six variables are instances of the class "Movie" from media.py
+# each of which represent the title, storyline, url for their poster image
+# as well as a link to their
+# youtube trailer. When run, this file will create
+# an html document alongside media.py and
+# fresh tomatoes.py that will display the name and
+# movie poster for each movie listed below.
+# If the image for a particular movie is clicked,
+# it will begin playing the youtube movie trailer for that film.
 
 toy_story = media.Movie("Toy Story",
                         "Movie about a boy and his toys that come to life.",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en"
+                        "/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://upload.wikimedia.org"
+                     "/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=6ziBFh3V1aM")
 
 burn_after_reading = media.Movie("Burn After Reading",
-                                 "Memoirs are stolen by idiots who think they are gov't secrets.",
-                                 "https://upload.wikimedia.org/wikipedia/en/4/42/Burn_After_Reading.png",
+                                 "Memoirs are stolen by idiots "
+                                 "who think they are gov't secrets.",
+                                 "https://upload.wikimedia.org"
+                                 "/wikipedia/en/4/42/Burn_After_Reading.png",
                                  "https://www.youtube.com/watch?v=SVCHSiRWjJM")
 
 o_brother = media.Movie("O'Brother Where Art Thou",
-                        "Three men break out of the chain gang and hilarity ensues",
-                        "https://upload.wikimedia.org/wikipedia/en/5/5b/O_brother_where_art_thou_ver1.jpg",
+                        "Three men break out of the "
+                        "chain gang and hilarity ensues",
+                        "https://upload.wikimedia.org/wikipedia"
+                        "/en/5/5b/O_brother_where_art_thou_ver1.jpg",
                         "https://www.youtube.com/watch?v=eW9Xo2HtlJI")
 
 shaun_of_the_dead = media.Movie("Shaun of the Dead",
-                               "Two idiots bumble through a zombie apocolypse.",
-                               "https://upload.wikimedia.org/wikipedia/en/thumb/e"
-                                "/ec/Shaun-of-the-dead.jpg/220px-Shaun-of-the-dead.jpg",
-                                "https://www.youtube.com/watch?v=LIfcaZ4pC-4")
-                                
+                                "Two idiots bumble through a zombie"
+                                "apocolypse.",
+                                "https://upload.wikimedia"
+                                ".org/wikipedia/en/thumb/e"
+                                "/ec/Shaun-of-the-dead."
+                                "jpg/220px-Shaun-of-the-dead.jpg",
+                                "https://www.youtube.com"
+                                "/watch?v=LIfcaZ4pC-4")
+
 fargo = media.Movie("Fargo",
                     "A husband's plan to profit goes terribly wrong.",
-                    "https://upload.wikimedia.org/wikipedia/en/3/33/Fargo_%281996_movie_poster%29.jpg",
-                    "https://www.youtube.com/watch?v=h2tY82z3xXU")
+                    "https://upload.wikimedia.org/"
+                    "wikipedia/en/3/33/Fargo_%281996_movie_poster%29.jpg",
+                    "https://www.youtube.com/"
+                    "watch?v=h2tY82z3xXU")
 
-                    
+movies = [toy_story, avatar, burn_after_reading,
+          o_brother, shaun_of_the_dead, fargo]
+# "movies" is a variable that
+# stores each instance of the class "Movie"
+fresh_tomatoes.open_movies_page(movies)
+# this code calls fresh_tomatoes.py and runs
+# through each instance of class "Movie" that is listed above. It fills
+# the information into the html document,
+# creating the movie trailer website.
 
-movies = [toy_story, avatar, burn_after_reading, o_brother, shaun_of_the_dead, fargo] #"movies" is a variable that stores each instance of the class "Movie"
-fresh_tomatoes.open_movies_page(movies) #this code calls fresh_tomatoes.py and runs through each instance of class "Movie" that is listed above. It fills
-                                        #the information into the html document, creatiing the movie trailer website. 
+
 
--- media.py
+++ media.py
@@ -1,21 +1,29 @@
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store information."""
-    #will show up as the __name__ variable ^
-    
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #example of a class variable
-    
+
+# will show up as the __name__ variable ^
+
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+# example of a class variable
+# all instances of class "Movie"
+# contain these ratings
+
     def __init__(self, movie_title, movie_storyline, poster_image_url,
                  trailer_youtube_url):
-        #def __init__ initializes the class movie and allows you to define
-        #attributes of the class
+        # def __init__ initializes the
+        # class movie and allows you to define
+        # attributes of the class for each
+        # instance created of that class (ie.
+        # each movie will have different attributes
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
-    def show_trailer(self): #a method that will open the youtube trailer for a specified movie
+    def show_trailer(self):
+        # a method that will open the youtube
+        # trailer for a specified movie
         webbrowser.open(self.trailer_youtube_url)
-    
-
